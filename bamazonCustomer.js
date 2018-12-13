15ebeabd685aeb1b6fcd550a6fa1ecfb26abeb36
Saryn Caister <thegreenmajority@gmail.com>
2018-12-12 21:18:31 -0500

    adding manager interfact inside original js file will break out later

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,11 +14,19 @@ let customerInterface = function() {
   connection.query(
     "SELECT item_id, product_name, price, stock_quantity FROM products",
     function(err, result, fields) {
-      if (err) console.log("Oops... Something went wrong");
-      console.log("*******************************************************");
-      console.log("WELCOME TO SARYN'S YUMMIES --- WHOLESALE ORDER PLATFORM");
-      console.log("PRODUCT ID --- PRODUCT NAME --- $PRICE/KG --- STOCK AVA");
-      console.log("*******************************************************");
+      if (err) console.log(" Oops... Something went wrong ");
+      console.log(
+        "***********************************************************"
+      );
+      console.log(
+        "* WELCOME TO SARYN'S YUMMIES --- WHOLESALE ORDER PLATFORM *"
+      );
+      console.log(
+        "*  PRODUCT ID -- PRODUCT NAME -- $PRICE/KG -- STOCK AVAL  *"
+      );
+      console.log(
+        "***********************************************************"
+      );
       for (i = 0; i < result.length; i++) {
         let nameWidth = 20;
         let priceWidth = 5;
@@ -84,9 +92,11 @@ let customerInterface = function() {
 };
 
 function onLoad() {
-  console.log("*******************************************************");
-  console.log("WELCOME TO SARYN'S YUMMIES --- WHOLESALE ORDER PLATFORM");
-  console.log("*******************************************************");
+  console.log(" ");
+  console.log("***********************************************************");
+  console.log("* WELCOME TO SARYN'S YUMMIES --- WHOLESALE ORDER PLATFORM *");
+  console.log("***********************************************************");
+  console.log(" ");
   connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
     inq
@@ -98,23 +108,23 @@ function onLoad() {
         },
         {
           name: "password",
-          type: "input",
-          message: "Password: "
+          type: "password",
+          message: "Password: ",
+          mask: true
         }
       ])
       .then(function(answer) {
-        if (
-          answer.loginID === "saryn" ||
-          ("Saryn" && answer.password === "manager")
-        ) {
+        if (answer.loginID === "saryn" && answer.password === "manager") {
+          console.log(" ");
+          console.log("Welcome Saryn!");
+          console.log("Loading Managers Interface...");
+          console.log(" ");
           managerInterface();
-        } else if (answer.loginID === "guest" && answer.password === "12345") {
+        } else if (answer.loginID === "guest") {
           console.log("");
           customerInterface();
         } else {
-          console.log(
-            "User ID or password combination not recognized please try again."
-          );
+          console.log("* User ID/Password combination not recognized. *");
           onLoad();
         }
       });
@@ -122,24 +132,157 @@ function onLoad() {
 }
 
 function managerInterface() {
-  console.log("Welcome Saryn!");
-  console.log("Loading Managers Interface");
   connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
     inq
       .prompt([
         {
           name: "managerOptions",
-          type: "rawlist",
+          type: "list",
           message: "Select A Report",
           choices: function() {
-            var pullReports = ["Current Products", "Low Inventory Alerts", "Add Inventory", "Add Product"];
+            var pullReports = [
+              "Current Products",
+              "Low Inventory Alerts",
+              "Add Inventory",
+              "Add Product"
+            ];
             return pullReports;
           }
         }
       ])
       .then(function(answer) {
-        console.log("it worked" + answer.managerOptions);
+        if (answer.managerOptions === "Current Products") {
+          connection.query("SELECT product_name FROM products", function(
+            err,
+            results
+          ) {
+            if (err) throw err;
+            console.log(results);
+            console.log(" ");
+            managerInterface();
+          });
+        } else if (answer.managerOptions === "Low Inventory Alerts") {
+          connection.query(
+            "SELECT product_name, stock_quantity FROM products",
+            function(err, results) {
+              if (err) throw err;
+              for (i = 0; i < results.length; i++) {
+                if (results[i].stock_quantity < 100) {
+                  console.log(results[i]);
+                }
+              }
+              console.log(" ");
+              managerInterface();
+            }
+          );
+        } else if (answer.managerOptions === "Add Inventory") {
+          if (err) throw err;
+          console.log(" ");
+          inq
+            .prompt([
+              {
+                name: "product_name",
+                type: "rawlist",
+                pageSize: 15,
+                choices: function() {
+                  var choiceArray = [];
+                  for (var i = 0; i < results.length; i++) {
+                    choiceArray.push(results[i].product_name);
+                  }
+                  return choiceArray;
+                },
+                message: "Which Product From Inventory To Edit?",
+                validate: function(value) {
+                  if (value === true) {
+                    return true;
+                  } else {
+                    return false;
+                  }
+                }
+              },
+              {
+                name: "stock_quantity",
+                type: "input",
+                message: "Submit New Inventory Level",
+                validate: function(value) {
+                  if (isNaN(value) === false) {
+                    return true;
+                  }
+                  console.log("error - must be a number");
+                  return false;
+                }
+              }
+            ])
+            .then(function(answer) {
+              let selectedProduct;
+              for (let i = 0; i < results.length; i++) {
+                if (results[i].product_name === answer.product_name) {
+                  selectedProduct = results[i];
+                }
+              }
+              let addInventory = function() {
+                connection.query(
+                  "UPDATE products SET ? WHERE ?",
+                  [
+                    {
+                      stock_quantity: answer.stock_quantity
+                    },
+                    {
+                      product_name: selectedProduct.product_name
+                    }
+                  ],
+                  function(err) {
+                    if (err) throw err;
+                  }
+                );
+                console.log("* Inventory Successfully Changed *");
+              };
+              addInventory();
+            });
+          managerInterface();
+        } else if (answer.managerOptions === "Add Product") {
+          if (err) throw err;
+          console.log(" ");
+          inq
+            .prompt([
+              {
+                name: "product_name",
+                type: "input",
+                message: "Submit Product Name"
+              },
+              {
+                name: "price",
+                type: "input",
+                message: "Submit $/kg"
+              },
+              {
+                name: "stock_quantity",
+                type: "input",
+                message: "Submit Inventory Level"
+              }
+            ])
+            .then(function(answer) {
+              connection.query(
+                "INSERT INTO products SET ?",
+                {
+                  product_name: product_name,
+                  price: price,
+                  stock_quantity: stock_quantity
+                },
+                function(err) {
+                  if (err) throw err;
+                  console.log("* Product Successfully Created *");
+                }
+              );
+            });
+          managerInterface();
+        } else {
+          console.clear();
+          console.log("Error: Input not recognized, please try again.");
+          console.log(" ");
+          managerInterface();
+        }
       });
   });
 }
@@ -188,7 +331,7 @@ function start() {
           return;
         }
         if (selectedProduct.stock_quantity <= 100) {
-          console.log("Stock Low Please Allow Extra Delivery Time");
+          console.log("* Stock Low - Extra Delivery Time Required *");
         }
         convertedQuan = [
           selectedProduct.stock_quantity - parseInt(answer.howMany)
@@ -235,15 +378,15 @@ function newOrder() {
       {
         name: "restart",
         type: "confirm",
-        message: "   ***      Place Another Order?       ***   "
+        message: "     ***      Place Another Order?       ***   "
       }
     ])
     .then(function(answer) {
       if (answer.restart === true) {
         customerInterface();
       } else {
-        console.log("   ***   Thank You For Your Business   ***   ");
-        console.log("   ***         See You Soon!           ***   ");
+        console.log("     ***   Thank You For Your Business   ***   ");
+        console.log("     ***         See You Soon!           ***   ");
         connection.end();
       }
     });
